cd288beb94ac22aa1b82261ea00d92200f5de79b
Red One <s890506@gmail.com>
2017-03-27 09:45:24 +0800

    Create 01-MetoriteGame.js

---
+++ 01-MetoriteGame.js
@@ -0,0 +1,99 @@
+let score = 0
+let pauseDifficulty = 0
+let meteorite2y = 0
+let meteorite2x = 0
+let meteorite1y = 0
+let meteorite1x = 0
+let shipLeftX = 0
+input.onButtonPressed(Button.AB, () => {
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # . .
+        . . . # .
+        . . # . .
+        . . . # .
+        . # # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    meteorite1x = Math.random(5)
+    meteorite1y = -4
+    meteorite2x = Math.random(6)
+    meteorite2y = -1
+    shipLeftX = 0
+    pauseDifficulty = 200
+    score = 0
+    led.plot(shipLeftX, 4)
+    led.plot(shipLeftX + 1, 4)
+    while (true) {
+        if (meteorite1y >= 0) {
+            led.unplot(meteorite1x, meteorite1y)
+        }
+        meteorite1y = meteorite1y + 1
+        if (meteorite1y >= 5) {
+            score = score + 1
+            meteorite1x = Math.random(5)
+            meteorite1y = -1
+        }
+        if (meteorite1y == 4 && (shipLeftX == meteorite1x || shipLeftX + 1 == meteorite1x)) {
+            basic.showString("GG")
+            basic.showString("S:")
+            basic.showNumber(score)
+            control.reset()
+        }
+        if (meteorite1y >= 0) {
+            led.plot(meteorite1x, meteorite1y)
+        }
+        if (meteorite2y >= 0) {
+            led.unplot(meteorite2x, meteorite2y)
+        }
+        meteorite2y = meteorite2y + 1
+        if (meteorite2y >= 5) {
+            score = score + 1
+            meteorite2x = Math.random(5)
+            meteorite2y = -1
+        }
+        if (meteorite2y == 4 && (shipLeftX == meteorite2x || shipLeftX + 1 == meteorite2x)) {
+            basic.showString("GG")
+            basic.showString("S:")
+            basic.showNumber(score)
+            control.reset()
+        }
+        if (meteorite2y >= 0) {
+            led.plot(meteorite2x, meteorite2y)
+        }
+        basic.pause(pauseDifficulty)
+    }
+})
+input.onButtonPressed(Button.A, () => {
+    if (shipLeftX >= 1) {
+        led.unplot(shipLeftX + 1, 4)
+        shipLeftX = shipLeftX - 1
+        led.plot(shipLeftX, 4)
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    if (shipLeftX <= 2) {
+        led.unplot(shipLeftX, 4)
+        shipLeftX = shipLeftX + 1
+        led.plot(shipLeftX, 4)
+        led.plot(shipLeftX + 1, 4)
+    }
+})
+basic.showIcon(IconNames.Heart)
